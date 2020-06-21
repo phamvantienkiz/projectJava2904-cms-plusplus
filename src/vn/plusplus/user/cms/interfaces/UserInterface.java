7be18b5445d8d2160c15a9bfbdccd679c291b2f9
kiemnx <kiemnguyenxuan@gmail.com>
2020-06-21 15:34:05 +0700

    Add interface

--- src/vn/plusplus/user/cms/interfaces/UserInterface.java
+++ src/vn/plusplus/user/cms/interfaces/UserInterface.java
@@ -1,4 +1,16 @@
 package vn.plusplus.user.cms.interfaces;
 
+import vn.plusplus.user.cms.model.User;
+
+import java.util.List;
+
 public interface UserInterface {
+    boolean checkUserByUserName(String userName);
+    List<User> readAllUserFromDB();
+    void saveUserToDB(User user);
+
+    User findUserByUserNameAndPassword(String userName, String passWord);
+
+    void sendEmailToResetPass(String email);
+
 }
--- src/vn/plusplus/user/cms/service/UserService.java
+++ src/vn/plusplus/user/cms/service/UserService.java
@@ -1,6 +1,33 @@
 package vn.plusplus.user.cms.service;
 
 import vn.plusplus.user.cms.interfaces.UserInterface;
+import vn.plusplus.user.cms.model.User;
+
+import java.util.List;
 
 public class UserService implements UserInterface {
+    @Override
+    public boolean checkUserByUserName(String userName) {
+        return false;
+    }
+
+    @Override
+    public List<User> readAllUserFromDB() {
+        return null;
+    }
+
+    @Override
+    public void saveUserToDB(User user) {
+
+    }
+
+    @Override
+    public User findUserByUserNameAndPassword(String userName, String passWord) {
+        return null;
+    }
+
+    @Override
+    public void sendEmailToResetPass(String email) {
+
+    }
 }
