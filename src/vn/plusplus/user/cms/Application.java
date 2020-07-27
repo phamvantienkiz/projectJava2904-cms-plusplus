720d9fd0af66d5824ee27fb53ec18b9e0f90db36
kiemnx <kiemnguyenxuan@gmail.com>
2020-07-27 21:33:47 +0700

    Add while in main

--- src/vn/plusplus/user/cms/Application.java
+++ src/vn/plusplus/user/cms/Application.java
@@ -8,30 +8,38 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) throws Exception{
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap lua chon su dung:");
-        System.out.println("1. Dang ky\n" +
-                "2. Dang nhap\n" +
-                "3. Reset password\n" +
-                "4. Tim kiem");
+        boolean ketThuc = true;
+        while (ketThuc){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Nhap lua chon su dung:");
+            System.out.println("1. Dang ky\n" +
+                    "2. Dang nhap\n" +
+                    "3. Reset password\n" +
+                    "4. Tim kiem\n" +
+                    "5. Ket thuc");
 
-        int option = scanner.nextInt();
-        UserService service = new UserService();
-        switch (option){
-            case 1:
-                System.out.println("Nhap username dang ky: ");
-                String userNameRegister = scanner.nextLine();
-                User userRe = service.findUserByUserName(userNameRegister);
-                if(userRe != null){
-                    System.out.println("Ten dang nhap da ton tai, moi ban chon ten khac");
-                } else {
-                    userRe = new User();
-                    System.out.println("Nhap vao fullName:");
-                    service.saveUserToDB(userRe);
-                }
-                break;
-            case 2:
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            UserService service = new UserService();
+            switch (option){
+                case 1:
+                    System.out.println("Nhap username dang ky: ");
+                    String userNameRegister = scanner.nextLine();
+                    User userRe = service.findUserByUserName(userNameRegister);
+                    if(userRe != null){
+                        System.out.println("Ten dang nhap da ton tai, moi ban chon ten khac");
+                    } else {
+                        userRe = new User();
+                        System.out.println("Nhap vao fullName:");
+                        service.saveUserToDB(userRe);
+                    }
+                    break;
+                case 2:
 
+                case 5:
+                    ketThuc = false;
+
+            }
         }
 
     }
